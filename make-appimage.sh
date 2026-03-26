9540a808e435485f16fcd08e60d9c9ddfec00812
Link4Electronics <link4electronics@gmail.com>
2026-03-26 07:13:29 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -14,7 +14,7 @@ export MAIN_BIN=photini
 export DEPLOY_PYTHON=1
 
 # Deploy dependencies
-quick-sharun /usr/bin/photini /usr/bin/exiftool
+quick-sharun /usr/bin/photini /usr/bin/exiftoolgui
 
 # Additional changes can be done in between here
 
