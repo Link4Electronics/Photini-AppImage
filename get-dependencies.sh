a8c5361e8dc14457ea66c1328b5513ea1c3f3bf8
Link4Electronics <link4electronics@gmail.com>
2026-03-26 06:12:32 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -8,6 +8,7 @@ echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
 pacman -Syu --noconfirm \
     go     \
+    nsight-compute \
     python
 
 echo "Installing debloated packages..."
